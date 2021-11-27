a27e8983de5ad85048d3ed93e796929569c8ddaf
FranczeskaP <patrycja.fraczkowska1@gmail.com>
2021-11-27 11:55:05 +0100

    Modbus update.

--- Modbus/ModbusMaster/Modbus.c
+++ Modbus/ModbusMaster/Modbus.c
@@ -16,53 +16,24 @@
 
 ModbusData_t modbusSensors;
 ModbusDataDc_t modbusDcSensors[numOfDcSlaves];
-struct timespec WaitTimeModbus = {
+static struct timespec WaitTimeModbus = {
     .tv_sec = 0,
-    .tv_nsec = TIME_MS_MODBUS * 1000000,
+    .tv_nsec = TIME_MS_MODBUS * 1000000
 };
-static uint8_t modbusRead(uint16_t slaveNum, uint16_t address, uint16_t numOfRegisters);
+static struct timeval response_timeout = {
+    .tv_sec = 1,
+    .tv_usec = 0
+};
+
+static uint8_t modbusReadDc(uint16_t slaveNum);
+static uint8_t modbusReadAc(void);
+static void initDcModbus(void);
+static void initAcModbus(void);
 
 void ModbusInit(void)
 {
-    struct timeval response_timeout;
-    response_timeout.tv_sec = 1;
-    response_timeout.tv_usec = 0;
-    for(uint16_t i = 0u; i < numOfDcSlaves; i++)
-    {
-        modbusSensors.dcSensors[i].slave = modbus_new_rtu(DEVICE_NAME, BAUDRATE, PARITY, DATA_BITS, STOP_BITS);
-        if (!modbusSensors.dcSensors[i].slave) 
-        {
-            fprintf(stderr, "Failed to create the context: %s\n", modbus_strerror(errno));
-            exit(1);
-        }
-
-        if (modbus_connect(modbusSensors.dcSensors[i].slave) == -1) 
-        {
-            fprintf(stderr, "Unable to connect: %s\n", modbus_strerror(errno));
-            modbus_free(modbusSensors.dcSensors[i].slave);
-            exit(1);
-        }
-        modbusSensors.dcSensors[i].slaveNum = i+1;
-        modbus_set_response_timeout(modbusSensors.dcSensors[i].slave, response_timeout.tv_sec, response_timeout.tv_usec);
-        modbus_set_slave(modbusSensors.dcSensors[i].slave, modbusSensors.dcSensors[i].slaveNum);
-    }
-    modbusSensors.acSensor.slave = modbus_new_rtu(DEVICE_NAME, BAUDRATE, PARITY, DATA_BITS, STOP_BITS);
-    if (!modbusSensors.acSensor.slave) 
-    {
-        fprintf(stderr, "Failed to create the context: %s\n", modbus_strerror(errno));
-        exit(1);
-    }
-
-    if (modbus_connect(modbusSensors.acSensor.slave) == -1) 
-    {
-        fprintf(stderr, "Unable to connect: %s\n", modbus_strerror(errno));
-        modbus_free(modbusSensors.acSensor.slave);
-        exit(1);
-    }
-    modbusSensors.acSensor.slaveNum = 5u;
-    modbus_set_response_timeout(modbusSensors.acSensor.slave, response_timeout.tv_sec, response_timeout.tv_usec);
-    modbus_set_slave(modbusSensors.acSensor.slave, modbusSensors.acSensor.slaveNum);
-
+    initDcModbus();
+    initAcModbus();
 }
 
 void ModbusDeInit(void)
@@ -83,15 +54,13 @@ uint8_t ModubsReadData(void)
     {
         if(configurationData[i].communicationProtocol = Modbus)
         {
-            if((configurationData[i].id != 0u) && (!configurationData[i].acSensor))
+            if((configurationData[i].id != 255u) && (!configurationData[i].acSensor))
             {
-                error |= modbusRead(configurationData[i].id, 0u, NUM_OF_DC_REGISTERS);
-                nanosleep(&WaitTimeModbus, &WaitTimeModbus);
+                error |= modbusReadDc(configurationData[i].id);
             }
-            else if((configurationData[i].id != 0u) && (configurationData[i].acSensor))
+            else if((configurationData[i].id != 255u) && (configurationData[i].acSensor))
             {
-                error |= modbusRead(configurationData[i].id, 0u, NUM_OF_AC_REGISTERS);
-                nanosleep(&WaitTimeModbus, &WaitTimeModbus);
+                error |= modbusReadAc();
             }
         }
     }
@@ -99,14 +68,71 @@ uint8_t ModubsReadData(void)
     return error;
 }
 
-static uint8_t modbusRead(uint16_t slaveNum, uint16_t address, uint16_t numOfRegisters)
+static uint8_t modbusReadDc(uint16_t slaveNum)
 {
     uint8_t error = 0u;
-    int numOfReadRegs = modbus_read_input_registers(modbusDcSensors[slaveNum-1].slave, address, numOfRegisters, modbusDcSensors[slaveNum-1].receivedData);
-    if (numOfReadRegs != numOfRegisters) 
+    int numOfReadRegs = modbus_read_input_registers(modbusSensors.dcSensors[slaveNum-1].slave, 0u, NUM_OF_DC_REGISTERS, modbusSensors.dcSensors[slaveNum-1].receivedData);
+    if (numOfReadRegs != NUM_OF_DC_REGISTERS) 
     {
         error = 1u;
         fprintf(stderr, "Failed to read: %s\n", modbus_strerror(errno));
     }
+    nanosleep(&WaitTimeModbus, &WaitTimeModbus);
     return error;
+}
+
+static uint8_t modbusReadAc(void)
+{
+    uint8_t error = 0u;
+    int numOfReadRegs = modbus_read_input_registers(modbusSensors.acSensor.slave, 0u, NUM_OF_AC_REGISTERS, modbusSensors.acSensor.receivedData);
+    if (numOfReadRegs != NUM_OF_AC_REGISTERS) 
+    {
+        error = 1u;
+        fprintf(stderr, "Failed to read: %s\n", modbus_strerror(errno));
+    }
+    nanosleep(&WaitTimeModbus, &WaitTimeModbus);
+    return error;
+}
+
+static void initDcModbus(void)
+{
+    for(uint16_t i = 0u; i < numOfDcSlaves; i++)
+    {
+        modbusSensors.dcSensors[i].slave = modbus_new_rtu(DEVICE_NAME, BAUDRATE, PARITY, DATA_BITS, STOP_BITS);
+        if (!modbusSensors.dcSensors[i].slave) 
+        {
+            fprintf(stderr, "Failed to create the context: %s\n", modbus_strerror(errno));
+            exit(1);
+        }
+
+        if (modbus_connect(modbusSensors.dcSensors[i].slave) == -1) 
+        {
+            fprintf(stderr, "Unable to connect: %s\n", modbus_strerror(errno));
+            modbus_free(modbusSensors.dcSensors[i].slave);
+            exit(1);
+        }
+        modbusSensors.dcSensors[i].slaveNum = i+1;
+        modbus_set_response_timeout(modbusSensors.dcSensors[i].slave, response_timeout.tv_sec, response_timeout.tv_usec);
+        modbus_set_slave(modbusSensors.dcSensors[i].slave, modbusSensors.dcSensors[i].slaveNum);
+    }
+}
+
+static void initAcModbus(void)
+{
+    modbusSensors.acSensor.slave = modbus_new_rtu(DEVICE_NAME, BAUDRATE, PARITY, DATA_BITS, STOP_BITS);
+    if (!modbusSensors.acSensor.slave) 
+    {
+        fprintf(stderr, "Failed to create the context: %s\n", modbus_strerror(errno));
+        exit(1);
+    }
+
+    if (modbus_connect(modbusSensors.acSensor.slave) == -1) 
+    {
+        fprintf(stderr, "Unable to connect: %s\n", modbus_strerror(errno));
+        modbus_free(modbusSensors.acSensor.slave);
+        exit(1);
+    }
+    modbusSensors.acSensor.slaveNum = 0u;
+    modbus_set_response_timeout(modbusSensors.acSensor.slave, response_timeout.tv_sec, response_timeout.tv_usec);
+    modbus_set_slave(modbusSensors.acSensor.slave, modbusSensors.acSensor.slaveNum);
 }
